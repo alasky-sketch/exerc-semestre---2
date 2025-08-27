deb10273a671a52ad83787369ea6b3dbcb20b80e
alasky-sketch <133282543+alasky-sketch@users.noreply.github.com>
2025-08-26 22:10:10 -0300

    Add files via upload

---
+++ aula2608.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <conio.h>
+
+struct pessoa{
+    char *nome;
+    int idade;
+    v
+};
+
+int main()
+{
+  struct pessoa cliente;
+  cliente.nome = "Oscar J. Silva";
+  cliente.idade = 36;
+  printf("O nome do cliente é %s\n", cliente.nome);
+  printf("A idade do cliente é %d\n", cliente.idade);
+    return 0;
+}
